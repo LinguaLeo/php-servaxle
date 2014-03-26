84f0ce31204737fac6bc99a935ecf149c71c89b3
Anthony Regeda <regeda@inbox.ru>
2014-03-26 13:05:59 +0400

    added license to top of php files

--- src/LinguaLeo/Servaxle/ClassLocator.php
+++ src/LinguaLeo/Servaxle/ClassLocator.php
@@ -1,5 +1,29 @@
 <?php
 
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 LinguaLeo
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 namespace LinguaLeo\Servaxle;
 
 use ReflectionClass;
@@ -12,6 +36,14 @@ class ClassLocator
     private $impls;
     private $links;
 
+    /**
+     * Instantiates the locator.
+     *
+     * @param array $sharings
+     * @param array $values
+     * @param array $impls
+     * @param array $links
+     */
     public function __construct(array $sharings = [], array $values = [], array $impls = [], array $links = [])
     {
         $this->sharings = $sharings;
@@ -20,21 +52,48 @@ class ClassLocator
         $this->links = $links;
     }
 
+    /**
+     * Gets an object.
+     *
+     * @param string $name
+     * @return object
+     * @throws \InvalidArgumentException
+     */
     public function __get($name)
     {
         if (empty($this->sharings[$name])) {
             throw new \InvalidArgumentException(sprintf('The sharing "%s" is undefined.', $name));
         }
-        return $this->$name = $this->newInstance(new ReflectionClass($this->sharings[$name]), $name);
+        return $this->$name = $this->createInstance($this->sharings[$name], $name);
     }
 
+    /**
+     * Creates a new instance from class name.
+     *
+     * @param string $className
+     * @param string $path
+     * @return object
+     */
+    public function createInstance($className, $path = '')
+    {
+        return $this->newInstance(new ReflectionClass($className), $path);
+    }
+
+    /**
+     * Creates a new instance from reflection.
+     *
+     * @param ReflectionClass $class
+     * @param string $path
+     * @return object
+     * @throws \RuntimeException
+     */
     private function newInstance(ReflectionClass $class, $path)
     {
         if (!$class->isInstantiable()) {
             if (empty($this->impls[$class->name])) {
                 throw new \RuntimeException(sprintf('No implementation found for "%s" in the path "%s".', $class->name, $path));
             }
-            return $this->newInstance(new ReflectionClass($this->impls[$class->name]), $path);
+            return $this->createInstance($this->impls[$class->name], $path);
         }
 
         $constructor = $class->getConstructor();
@@ -45,6 +104,14 @@ class ClassLocator
         return $class->newInstanceArgs($this->getArguments($constructor, $path));
     }
 
+    /**
+     * Returns arguments for the constructor.
+     *
+     * @param ReflectionMethod $constructor
+     * @param string $path
+     * @return array
+     * @throws \RuntimeException
+     */
     private function getArguments(ReflectionMethod $constructor, $path)
     {
         $args = [];
@@ -63,10 +130,17 @@ class ClassLocator
         return $args;
     }
 
+    /**
+     * Reflects and returns a value from a path.
+     *
+     * @param mixed $value
+     * @param string $path
+     * @return mixed
+     */
     private function reflectValue($value, $path)
     {
         if (is_string($value) && class_exists($value)) {
-            $value = $this->newInstance(new ReflectionClass($value), $path);
+            $value = $this->createInstance($value, $path);
         }
         if (is_callable($value)) {
             $value = $value($this);
